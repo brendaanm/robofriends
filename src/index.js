7991c71ee6c05eb6b4d2581f507bd63f4fc30b3c
Margetts <brendan.margetts@gmail.com>
2021-07-21 13:23:40 +1000

    upload robofriends

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+import 'tachyons';
+ReactDOM.render(<App />, document.getElementById('root'));
+/* <div>
+<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+<Card id={robots[1].id} name={robots[1].name} email={robots[2].email}/>
+<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+<Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+</div> */
+
+
